725ee710b91a75e5b8d7f229957ab10d2e47369e
sairajvardhan <sairajvardhandamisetti@gmail.com>
2025-12-12 10:13:06 +0530

    pem added in env

--- aiven_deploy/aiven_deploy/settings.py
+++ aiven_deploy/aiven_deploy/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'PORT':os.getenv("DB_PORT"),
         "OPTIONS":{
             "ssl":{
-                "ca":os.path.join(BASE_DIR, "aiven_ca.pem"),
+                "ca": os.getenv("PEM_CONT"),
             },
             'charset':'utf8mb4',
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
