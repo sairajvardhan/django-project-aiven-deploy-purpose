e6c11b4b575d22a7589e1aadec435532c8bd74d7
sairajvardhan <sairajvardhandamisetti@gmail.com>
2025-12-12 11:30:39 +0530

    host added

--- aiven_deploy/aiven_deploy/settings.py
+++ aiven_deploy/aiven_deploy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-project-aiven-deploy-purpose-9l2w.onrender.com']
 
 
 # Application definition
